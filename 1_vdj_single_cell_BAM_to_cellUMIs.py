58d1145a8b5fabeb35ae85cea7b519818cab0ca8
RaviDandekar <ravi.dandekar@gmail.com>
2020-03-20 10:26:43 -0700

    made <1_vdj_single_cell_BAM_to_cellUMIs.py> more user friendly

--- 1_vdj_single_cell_BAM_to_cellUMIs.py
+++ 1_vdj_single_cell_BAM_to_cellUMIs.py
@@ -3,16 +3,15 @@ import sys,time,re,os
 import numpy  as np
 import pandas as pd
 import pysam
-import python_functions as pf
+import pathlib
+from optparse import OptionParser
+import support_functions as pf
 from multiprocessing import Pool
 
 
-def extract_cellumi(x):
-  sample = x.split('/')[-3]
-  print(sample)
-  
-  # make results file
-  results_dir = "/data/rdandekar/rprojects/sc_analysis_2019/contamination_analysis/vdj_contamination_analysis/decontam_Jan2020/results_all_BCR/round1/CellUMI_files/"
+def extract_cellumi(fh_tup):
+  bam_fh,results_dir = fh_tup
+  sample = bam_fh.split('/')[-3]
   sample_cellumi_fh = results_dir + sample + '.csv'
   
   print("CREATING:\t",sample_cellumi_fh)
@@ -20,7 +19,7 @@ def extract_cellumi(x):
   OUT = open(sample_cellumi_fh,'w')
   OUT.write("read,cell_barcode,umi_seq\n")
   
-  samfile = pysam.AlignmentFile(x, "rb")
+  samfile = pysam.AlignmentFile(bam_fh, "rb")
   for read in samfile.fetch():
     read_id = read.qname
     tag_array = read.tags
@@ -39,31 +38,44 @@ def extract_cellumi(x):
   
   OUT.close()
   
+  
 
 if __name__ == '__main__':
-  THREADS = 10
+  usage = "\n<PATH>/%prog -h"
+  parser = OptionParser(usage=usage)
+  parser.add_option("-i", "--input", default = False, dest="i", help="Input Cellranger VDJ Results Directory, Default = False")
+  parser.add_option("-o", "--output", default = "./results", dest="o", help="Output Directory, Default = ./results")
+  parser.add_option("-t", "--threads", default = 1, dest="t", help="Thread count, Default = 1")
+  (opt, args) = parser.parse_args()
+
+  input_vdj_dir    = opt.i
+  results_dir      = opt.o    # Results directory hub
+  THREADS          = opt.t    # Threads
+
+  if opt.i == False:
+	  sys.exit("\nInvalid Arguements! Must use [-i], option [-o] recommended. Use [-h] for help\n")
   
   # Create list of BAM files
   bam_list = []
   sample_hash = pf.AutoVivification()
-  vdj_dir_bcr_new     = "/data/ryan/10x/2020_vdj_fastqs/2020_vdj_results/BCR/"
-  vdj_dir_bcr_old     = "/data/rdandekar/Reruns_cellranger/All_VDJ_cellranger_results_Dec2018/"
-  results_dir = "/data/rdandekar/rprojects/sc_analysis_2019/contamination_analysis/vdj_contamination_analysis/decontam_Jan2020/results_all_BCR/round1/CellUMI_files/"
+  #vdj_dir_bcr_new     = "/data/ryan/10x/2020_vdj_fastqs/2020_vdj_results/BCR/"
+  #vdj_dir_bcr_old     = "/data/rdandekar/Reruns_cellranger/All_VDJ_cellranger_results_Dec2018/"
+  #results_dir = "/data/rdandekar/rprojects/sc_analysis_2019/contamination_analysis/vdj_contamination_analysis/decontam_Jan2020/results_all_BCR/round1/CellUMI_files/"
+  
+  # Create nested Results Directory
+  if results_dir[-1:] != '/':
+    results_dir += '/'
+  results_dir += "CellUMI_files/"
   if os.path.isdir(results_dir) == False:
-    os.mkdir(results_dir)
-    
-  dir_samples_bcr_new = os.listdir(vdj_dir_bcr_new)
-  for sample in dir_samples_bcr_new:
-    bam_file = vdj_dir_bcr_new + sample + "/outs/all_contig.bam"
-    sample_hash[sample] = bam_file
-    bam_list.append(bam_file)
+    pathlib.Path(results_dir).mkdir(parents=True, exist_ok=True)
+    #os.mkdir(results_dir)
   
-  dir_samples_bcr_old = os.listdir(vdj_dir_bcr_old)
-  for sample in dir_samples_bcr_old:
-    bam_file = vdj_dir_bcr_old + sample + "/outs/all_contig.bam"
+  dir_samples = os.listdir(input_vdj_dir)
+  for sample in dir_samples:
+    bam_file = input_vdj_dir + sample + "/outs/all_contig.bam"
     sample_hash[sample] = bam_file
-    bam_list.append(bam_file)
-  
+    bam_tup = (bam_file,results_dir)
+    bam_list.append(bam_tup)
   
   ## Extract cell barcode UMI info from BAM files
   with Pool(THREADS) as p:
